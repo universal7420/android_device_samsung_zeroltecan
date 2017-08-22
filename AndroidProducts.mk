7b6f244e7bdd72a9043142dae089d249af5e8ea0
Lukas0610 <mail@lukasberger.at>
2017-08-22 02:38:33 +0200

    zero: added support for pure AOSP

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_zeroltecan.mk \
     $(LOCAL_DIR)/aokp.mk \
+    $(LOCAL_DIR)/aosp_zeroltecan.mk \
     $(LOCAL_DIR)/lineage.mk \
     $(LOCAL_DIR)/omni_zeroltecan.mk
