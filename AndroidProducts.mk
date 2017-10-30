be99bce18509baed80a028b8b259c9ccee90d0a4
Lukas0610 <mail@lukasberger.at>
2017-10-30 17:45:49 +0100

    zeroltecan: added product-makefile for GZOSP
    Change-Id: I7df6ea627c22acedfa1c595a1c0cac761acbe5f4

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -2,5 +2,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_zeroltecan.mk \
     $(LOCAL_DIR)/aokp.mk \
     $(LOCAL_DIR)/aosp_zeroltecan.mk \
+    $(LOCAL_DIR)/gzosp.mk \
     $(LOCAL_DIR)/lineage.mk \
     $(LOCAL_DIR)/omni_zeroltecan.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,5 +16,6 @@
 
 add_lunch_combo aokp_zeroltecan-userdebug
 add_lunch_combo aosp_zeroltecan-userdebug
+add_lunch_combo gzosp_zeroltecan-userdebug
 add_lunch_combo lineage_zeroltecan-userdebug
 add_lunch_combo omni_zeroltecan-userdebug
