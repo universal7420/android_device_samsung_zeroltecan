86f364136acce2106384d51a2c5e382ff54e079a
enesuzun2002 <enesuzun200227@gmail.com>
2020-03-08 21:20:55 +0100

    zeroltecan: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_zeroltecan.mk 
+    $(LOCAL_DIR)/lineage_zeroltecan.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_zeroltecan-user \
+    lineage_zeroltecan-userdebug \
+    lineage_zeroltecan-eng
